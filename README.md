7ada31b7f8bea7624d00f622f6e945479c550119
chenpx976 <chenpx976@gmail.com>
2018-11-23 11:34:03 +0800

    feat: Refactoring interface parameters && Support the json data and string

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ npm i -S lottie-compress
 import LottieCompress from 'lottie-compress';
 
 async () => {
-  const lottieCompress = new LottieCompress(JSON.parse(data));
+  const lottieCompress = new LottieCompress(lottieJson);
   const ret = await lottieCompress.execute();
   console.log('ret', ret);
 }
@@ -20,4 +20,5 @@ async () => {
 
 ## Options
 
-- `lottieJson` Lottie file json data, Please transform the data `JSON.parse(data)` before use.
+- `lottieJson` Lottie file json data, Support the json data and string.
+- `options` not support.
--- test/index.test.ts
+++ test/index.test.ts
@@ -6,9 +6,16 @@ import * as path from 'path';
 const data = fs.readFileSync(path.resolve(__dirname, 'fixtures/rockball.json')).toString();
 
 describe('LottieCompress', () => {
-  it('it is ok', async () => {
+  it('lottieJson is lottie string', async () => {
+    const lottieCompress = new LottieCompress(data);
+    const ret = await lottieCompress.execute();
+    // console.log('ret', ret);
+    assert(ret.tiny === '75');
+  });
+  it('lottieJson is lottie json object', async () => {
     const lottieCompress = new LottieCompress(JSON.parse(data));
     const ret = await lottieCompress.execute();
-    console.log('ret', ret);
+    // console.log('ret', ret);
+    assert(ret.tiny === '75');
   });
 });
